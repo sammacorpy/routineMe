900c8a937307253bd6983a7ac3b1eb4a0ec4ee5c
Ritesh <riteshsinghgzp2016@gmail.com>
2022-01-18 16:17:04 +0530

    removed ffi dependency

--- lib/components/navicons.dart
+++ lib/components/navicons.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:routineme/devicesettings/devicesettings.dart';
 
@@ -8,21 +6,33 @@ class NavIcons extends StatelessWidget {
   final String? text;
   final Color? color;
   final Color? bgColor;
-  final Bool? active;
-  const NavIcons({Key? key, required this.iconData, this.text, this.color, this.bgColor, this.active}): super(key: key);
+  final bool? active;
+  const NavIcons(
+      {Key? key,
+      required this.iconData,
+      this.text,
+      this.color,
+      this.bgColor,
+      this.active})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Icon(iconData, size: DeviceSettings.textScaleFactor(context) * 23, color: color,),
-        if(text != null) 
-        Text(text as String,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: DeviceSettings.textScaleFactor(context)*10),
-        )
+        Icon(
+          iconData,
+          size: DeviceSettings.textScaleFactor(context) * 23,
+          color: color,
+        ),
+        if (text != null)
+          Text(
+            text as String,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: DeviceSettings.textScaleFactor(context) * 10),
+          )
       ],
     );
   }
-  
 }
