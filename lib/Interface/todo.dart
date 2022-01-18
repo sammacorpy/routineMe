ea6cab3e5291a4a141d3dcb75de667f87266934f
Ritesh <riteshsinghgzp2016@gmail.com>
2022-01-18 15:52:15 +0530

    wip

--- lib/Interface/todo.dart
+++ lib/Interface/todo.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:../models/task.dart';
- class todo extends StatefulWidget {
+import '../models/task.dart';
+ class Todo extends StatefulWidget {
     String appBarTitle;
    Task task;
   todo_state todoState;
   new_task(this.task, this.appBarTitle, this.todoState);
   bool _isEditingText = false;
 
-  @override
-  State<StatefulWidget> createState() {
+  @override  //Decorator
+    State<StatefulWidget> createState() {
     return task_state(this.task, this.appBarTitle, this.todoState);
   }
 }
