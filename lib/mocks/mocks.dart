5cfbc1f471f005512d22629f4af255b4dc4e1fdf
Shivam <sammacorpy@gmail.com>
2022-01-28 01:28:48 +0800

    fixed mocks

--- lib/mocks/mocks.dart
+++ lib/mocks/mocks.dart
@@ -2,54 +2,69 @@ import 'package:routineme/models/priority.dart';
 import 'package:routineme/models/todo.dart';
 
 class Mocks {
-  static const List<TodoModel> todos = [
+  static List<TodoModel> todos = [
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
+
     TodoModel(
       active: true,
       id: 'id1',
-      name: 'Clear the basket for laundry',
+      title: 'Clear the basket for laundry',
       priority: Priority.low,
+      createdAt: DateTime.now(),
     ),
   ];
 }
